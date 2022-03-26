52aa62715460bc9a627affacd19eae0d7622036e
ghpark <pkh879@gmail.com>
2022-03-26 15:04:46 +0900

    [ADD] create project for spring webflux

---
+++ src/main/java/com/ghpark/demo/SpringVideoStreamDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ghpark.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVideoStreamDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVideoStreamDemoApplication.class, args);
+    }
+
+}
